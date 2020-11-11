80b6953630c174489d64eb26de5d97b53678ebbc
Zeddelix <60432340+Zeddelix@users.noreply.github.com>
2020-11-11 14:39:42 +0100

    Revert "Add files via upload"
    This reverts commit f8bd50ecd360216f7afa607daaebe627c78f43e5.

--- afficheuretiquette.cpp
+++
@@ -1,4 +0,0 @@
-#include"afficheuretiquette.h"
-
-afficheurEtiquette::~afficheurEtiquette()
-{}
--- afficheuretiquetteHTML.h
+++
@@ -1,7 +0,0 @@
-#include"afficheuretiquette.h"
-
-class afficheurEtiquetteHTML : public afficheurEtiquette {
-public:
-  virtual void afficheEtiquette(const colis& c, std::ostream& ost) const override;
-  virtual void afficheEtiquetteExpress(const colisExpress& c, std::ostream& ost) const override;
-};
